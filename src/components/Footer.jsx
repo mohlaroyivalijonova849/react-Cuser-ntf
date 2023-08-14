554cb5511d0f5d6ead07c951d9274d0deee170f1
mohlaroyivalijonova849 <mokhlaroyim.valijonova@gmail.com>
2023-08-14 11:36:10 +0500

    react user

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container container">
+        <p>
+          All Right Reserved.{" "}
+          <a href="https://github.com/mohlaroyivalijonova849">Mohlaroyim</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
